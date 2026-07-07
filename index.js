d24c266d424608b1183ac28408c0194ed2c8e5c7
GTWasTaken <malcolmlim12345@gmail.com>
2026-07-07 11:31:46 +0800

    added crypto polyfill for ethers support

---
+++ index.js
@@ -0,0 +1,7 @@
+import "@ethersproject/shims";
+import { Buffer } from "buffer";
+import "react-native-get-random-values";
+global.Buffer = Buffer;
+
+import "expo-router/entry";
+
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,12 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('wasm');
+config.resolver.extraNodeModules = {
+  crypto: require.resolve("react-native-crypto"),
+  stream: require.resolve("stream-browserify"),
+  buffer: require.resolve("buffer"),
+};
+config.resolver.assetExts.push("wasm");
 module.exports = config;
